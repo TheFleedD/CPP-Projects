8ff190d1bac781c3b4b0469d47a2d85d461b0e9c
FleedD <mrfleedd5159@gmail.com>
2022-11-08 20:59:31 +0300

    Зд по фуцнкциям 2 готово

---
+++ C++/Зд по функциям 2/Зд по функциям 2.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <Windows.h>
+
+float func1(float x) {
+    return 4 * pow(x, 2) + (2 * x) - 19;
+}
+float func2(float x) {
+    return ((2 * x) / (-4 * x + 1));
+}
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    float x = 0;
+    std::cout << "Введите X =";
+    std::cin >> x;
+    if (x >= 3.5) {
+        std::cout << func1(x);
+    }
+    else {
+        std::cout << func2(x);
+    }
+}
