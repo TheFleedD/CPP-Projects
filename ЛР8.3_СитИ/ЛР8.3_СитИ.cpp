073e9338352845954da48a3df7ff6fee36f9f17c
FleedD <mrfleedd5159@gmail.com>
2022-11-09 20:26:09 +0300

    Сделал задание 8 v1

---
+++ ЛР8.3_СитИ/ЛР8.3_СитИ.cpp
@@ -0,0 +1,28 @@
+﻿using namespace std;
+#include <iostream>
+#include <Windows.h>
+#include <string>
+#include <conio.h> 
+#include <stdlib.h> 
+#include <time.h> 
+#include <iomanip>
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    string s1;
+    cout << "Введите строчку = ";
+    getline(cin,s1);
+    int k = s1.size();
+    for (int i=s1.size();i>0;i--)
+    {
+        char c = s1[i];
+        if (c == ' ')
+        {
+            k = k - i;
+            break;
+        }
+    }
+    cout << "Количество символов в последнем слове строки > " << k - 1 << endl;
+}
+
