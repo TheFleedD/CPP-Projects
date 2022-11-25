fcdade6c93a27ccc59a7dcddc57e4c076761fa6b
FleedD <mrfleedd5159@gmail.com>
2022-11-25 16:28:34 +0300

    Зд 1 по ООП

---
+++ Зд по ООП 1/Зд по ООП 1.cpp
@@ -0,0 +1,63 @@
+﻿#include <iostream>
+#include <Windows.h>
+#include <string>
+
+using namespace std;
+
+struct Irrlt
+{
+private:
+    double first, second;
+
+public:
+    Irrlt()
+    {
+        first = 0;
+        second = 0;
+    }
+    Irrlt(double first, double second)
+    {
+        this->first = first;
+        this->second = second;
+    }
+
+    void Read()
+    {
+        cout << "Введите число А" << endl;
+        cin >> first;
+        while (first == 0)
+        {
+            cin.clear();
+            cout << "Введите число Б не равное 0 =" << endl;
+            cin >> first;
+        }
+        cout << "Введите число Б" << endl;
+        cin >> second;
+        while (second == 0)
+        {
+            cin.clear();
+            cout << "Введите число Б не равное 0 =" << endl;
+            cin >> second;
+        }
+    }
+    void Display()
+    {
+        cout << "Число А =" << first << endl;
+        cout << "Число Б =" << second << endl;
+        cout << "Корень =" << Irrlt::Root() << endl;
+    }
+    double Root()
+    {
+        return (-second / first);
+    }
+};
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    Irrlt ir;
+    ir.Read();
+    cout << "Кор =" << ir.Root() << endl;
+    ir.Display();
+}
