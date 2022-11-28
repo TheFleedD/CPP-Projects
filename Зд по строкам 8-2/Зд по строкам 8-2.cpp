dcb988bbddf93f8df31d7a4e0ac37aa1a754c9f2
FleedD <mrfleedd5159@gmail.com>
2022-11-28 22:04:26 +0300

    Lr_8-2 Ситчихин С

---
+++ Зд по строкам 8-2/Зд по строкам 8-2.cpp
@@ -0,0 +1,41 @@
+﻿//Зд 22
+#include <iostream>
+#include <Windows.h>
+#include <string>
+
+using namespace std;
+
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    
+    string str = "", word = "";
+    int k = 0;
+    bool t = false;
+
+    cout << "Введите строку=";
+    cin >> str;
+    cout << "Введите слово=";
+    cin >> word;
+    for (int i = 0; i < str.size() - word.size() + 1; i++)
+    {
+        t = false;
+        for (int j = 0; j < word.size(); j++)
+        {
+            if (str[i + j] != word[j])
+            {
+                break;
+            }
+            else
+            {
+                t = true;
+            }
+        }
+        if (t)
+            k++;
+    }
+    cout << "Слово встречено " << k << " раз";
+    return 0;
+}
